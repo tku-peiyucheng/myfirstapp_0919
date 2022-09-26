830303b932b8b3627e686325e51671d3b2eb4bd9
tku-peiyucheng <100709337+tku-peiyucheng@users.noreply.github.com>
2022-09-26 13:42:57 +0800

    add pic

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   // 建立appTitle和hiFlutter物件
   var appTitle = Text('我的第一個Flutter App'),
-      hiFlutter = Text('Hi, Flutter.',
-        style: TextStyle(fontSize: 50),
+      hiFlutter = Text("Hi, Flutter.",
+        style: TextStyle(fontSize: 30),
       );
 
   // 建立appBody物件
@@ -15,6 +15,7 @@ void main() {
   // 建立appBar物件
   var appBar = AppBar(
     title: appTitle,
+    backgroundColor: Color(0xff0000ff),
   );
 
   // 建立app物件
@@ -22,6 +23,7 @@ void main() {
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+      backgroundColor: Colors.yellow
     ),
   );
 
